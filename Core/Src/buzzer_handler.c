c0a7d0015c3fa8c2b01cb9c303f9af03614e26ad
pl_olov2 <ak.dziagwa@gmail.com>
2026-03-02 15:33:06 +0100

    quick buzz update

--- Core/Src/buzzer_handler.c
+++ Core/Src/buzzer_handler.c
@@ -1,11 +1,10 @@
 /* Copyright 2025 PWR Racing Team
  *
- * This file contains the heat and fan handler task 
+ * This file contains the buzzer handler task 
  * implementation. This code is run as a FreeRTOS task.
  *
- * The handler is responsible for processing the heating 
- * logic and driving the physical GPIO pins for the 
- * heating element and the fan based on current sensor data.
+ * The handler is responsible for playing "music" via buzzer.
+ * It should play on startup, when stage ends or when error ocures. 
  */
 
 #include "cmsis_os2.h"
@@ -15,11 +14,6 @@
 #include "stm32f4xx_hal_gpio.h"
 #include <stdint.h>
 
-/**
- * External function to report system errors in a thread-safe manner.
- */
-void sett_error(uint8_t error_value, uint16_t* error_codes, osSemaphoreId_t error_sem);
-
 typedef struct{
     uint16_t frequency;
     uint16_t duration;
@@ -27,8 +21,24 @@ typedef struct{
 
 typedef struct{
     uint8_t num_notes;
-    Note* notes;
+    const Note* notes;
 } Melody;
+
+static const Melody R2D = {
+    .num_notes = 6,
+    .notes = (Note[]) {
+        {1047, 500},
+        {1319, 500},
+        {1047, 500},
+        {1319, 500},
+        {1047, 500},
+        {1319, 500}
+    }
+};
+static const Melody stage_end;
+static const Melody baking_end;
+static const Melody error;
+
 
 void buzzer_handler(void* argument){
 
